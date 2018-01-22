9707098082620b35d2693431040e18baf8e5b37c
Dennis Padiernos <depadiernos@gmail.com>
2018-01-21 22:16:13 -0600

    implement header/footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 export default class Footer extends Component {
   render() {
     return (
-      <div>
+      <FootContainer>
         <CenterP>
           <strong>St. Thomas Aquinas Catholic Church</strong><br/>
           421 North Washington Avenue, Cookeville, TN 38501<br/>
@@ -12,14 +12,17 @@ export default class Footer extends Component {
         <CenterP>
           Phone: 931-526-2575<br/>
           Fax: 931-526-5869<br/>
-          Email:stthomasaquinas@citlink.net
+          Email: stthomasaquinas@citlink.net
         </CenterP>
-      </div>
+      </FootContainer>
     );
   }
 }
 
+const FootContainer = styled.div`
+  background-color: #3b5998;
+  color: white;
+`
 const CenterP = styled.p `
-    text-align: center;
-    color:;
+  text-align: center;
 `
--- src/components/header.js
+++ src/components/header.js
@@ -11,17 +11,16 @@ const Header = () => {
       <HeaderGrid >
         <Logo src={logo} alt='logo'/>
         <Social>
-            <DonateBtn>
-              <a
-              href='https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=LlvaBlCXmbvIkUuFAt-bgG4n1bx7EGDfXvacmoYbpOHZrUJlPQiCC6Aznn3HQnXMDok6z3A8wOdiKHYzjfh7oNnZWltx2hpQpEq9Hc9FikrVwg6DoI1HaYuhBdu6ZZZfOAP9FdQAJOc-0hY_z1RsTxirfa8OUqd_8wtT_bKFXIw5JFP5mKoFPm1O-u1W3fe0JQrfyRl2Gi87noGl1RDR5w==&ver=3'>
-                Support Our Church
-              </a>
-            </DonateBtn>
-            <FacebookBtn>
-              <a href='https://facebook.com/saintthomasaquinascookeville'>
-                <i className='fa fa-facebook' aria-hidden='true'></i>
-              </a>
-            </FacebookBtn>
+          <DonateBtn>
+            <a href='https://www.eservicepayments.com/cgi-bin/Vanco_ver3.vps?appver3=LlvaBlCXmbvIkUuFAt-bgG4n1bx7EGDfXvacmoYbpOHZrUJlPQiCC6Aznn3HQnXMDok6z3A8wOdiKHYzjfh7oNnZWltx2hpQpEq9Hc9FikrVwg6DoI1HaYuhBdu6ZZZfOAP9FdQAJOc-0hY_z1RsTxirfa8OUqd_8wtT_bKFXIw5JFP5mKoFPm1O-u1W3fe0JQrfyRl2Gi87noGl1RDR5w==&ver=3'>
+              Support Our Church
+            </a>
+          </DonateBtn>
+          <FacebookBtn>
+            <a href='https://facebook.com/saintthomasaquinascookeville'>
+              <i className='fa fa-facebook' aria-hidden='true'></i>
+            </a>
+          </FacebookBtn>
         </Social>
       </HeaderGrid>
       <Nav>
@@ -63,11 +62,13 @@ const Header = () => {
 export default Header;
 
 const HeaderGrid = styled(Grid)`
+grid-template-columns: 1fr 1fr 1fr;
 background: #FDFCF8;
 margin: 0 !important;`
 
 const Logo = styled.img`
 display: flex;
+grid-column: 2 / 3;
 justify-center: center;
 margin: 0 auto;
 max-width: 350px;`
@@ -84,7 +85,13 @@ li {
 }`
 
 const NavLink = styled(Link)`
-font-size: 1em;`
+font-size: 1em;
+text-decoration: none;
+:hover {
+  background-color: #3b5998;
+  color: white;
+}
+`
 
 const Social = styled.div `
 display:flex;
