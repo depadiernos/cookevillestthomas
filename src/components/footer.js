a4c00051a9bec20991e741499bc70bfa5103df3a
Dennis Padiernos <depadiernos@gmail.com>
2018-01-22 10:54:01 -0600

    style footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,26 +1,27 @@
-import React, {Component} from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <FootContainer>
-        <CenterP>
-          <strong>St. Thomas Aquinas Catholic Church</strong><br/>
-          421 North Washington Avenue, Cookeville, TN 38501<br/>
-        </CenterP>
-        <CenterP>
-          Phone: 931-526-2575<br/>
-          Fax: 931-526-5869<br/>
-          Email: stthomasaquinas@citlink.net
-        </CenterP>
-      </FootContainer>
-    );
-  }
+const Footer = () => {
+  return (
+    <FootContainer>
+      <CenterP>
+        <strong>St. Thomas Aquinas Catholic Church</strong><br/>
+        421 North Washington Avenue, Cookeville, TN 38501<br/>
+      </CenterP>
+      <CenterP>
+        Phone: 931-526-2575<br/>
+        Fax: 931-526-5869<br/>
+        Email: stthomasaquinas@citlink.net
+      </CenterP>
+    </FootContainer>
+  );
 }
 
+export default Footer;
+
 const FootContainer = styled.div`
-  background-color: #3b5998;
+  background-color: #262161;
+  padding: 10px 0;
   color: white;
 `
 const CenterP = styled.p `
